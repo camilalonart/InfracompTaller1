966a292db88430f5c772e82e94e9d248579128f4
Mc Londono <mc.londono@uniandes.edu.co>
2019-09-03 13:10:00 -0500

    servidor

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,4 +1,3 @@
-
 public class Servidor extends Thread {
 	private Buffer buffer;
     public Servidor (Buffer b ){
@@ -10,6 +9,28 @@ public class Servidor extends Thread {
 	public void setBuffer(Buffer buffer) {
 		this.buffer = buffer;
 	}
-    
-	public void enviarMensaje() {}
+	
+	@Override
+    public void run(){
+		while(true){
+			Mensaje m = buffer.sacarMensajes();
+			if(m != null){
+				synchronized (m){
+					m.setRespondido(true);
+					buffer.setMensajesFinalizados(buffer.getMensajesRestantes()-1);
+					System.out.println("Respuesta : "+ (m.getMensaje()));
+					m.notify();
+				}
+			}
+			if(buffer.getMensajesRestantes() == 0){
+				try {
+					System.out.println("TERMINO serv. "+ this.hashCode());
+					join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			yield();
+		}
+    }
 }
